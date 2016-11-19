f0a69a1c9725ee3a722456d5ded79e4eb6324be4
Jamie Allen <Jamieallen59@gmail.com>
2016-11-19 22:10:04 +0000

    cleaned up images output

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,78 +1,80 @@
-import path from 'path';
-import webpack from 'webpack';
-import ExtractTextPlugin from "extract-text-webpack-plugin";
+import path from 'path'
+import webpack from 'webpack'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 // use an index.js for these!
-import HtmlWebpackPluginConfig from './utils/HtmlWebpackPlugin';
-import CopyWebpackPluginConfig from './utils/CopyWebpackPlugin';
+import HtmlWebpackPluginConfig from './utils/HtmlWebpackPlugin'
+import CopyWebpackPluginConfig from './utils/CopyWebpackPlugin'
 
-const env = process.env.NODE_ENV;
+const env = process.env.NODE_ENV
 
 const config = {
-    entry: [
-        './src/js/app.js'
-    ],
-    output: {
-        path: './',
-        filename: 'index_bundle.js'
-    },
-    devServer: {
+	entry: [
+		'./src/js/app.js'
+	],
+	output: {
+		path: './',
+		filename: 'index_bundle.js'
+	},
+	devServer: {
 		hot: true,
 		inline: true,
-        // This is required for webpack-dev-server. The path should
-        // be an absolute path to the build destination.
-        outputPath: path.join(__dirname + '/'),
-        stats: {
-            chunks: false
-        }
-    },
-    devtool: 'source-map',
-    module: {
-        preLoaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loaders: ['eslint-loader']
-        }],
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loaders: ['react-hot', 'babel-loader']
-        }, {
-            test: /\.(css|less)$/,
-            exclude: [ /src(\\|\/)js/ ],
-            loader: ExtractTextPlugin.extract("style-loader", "css?sourceMap!less?sourceMap")
-        }, {
-            test: /\.(css|less)$/,
-            include: [ /src(\\|\/)js/ ],
-            loader: ExtractTextPlugin.extract('style-loader', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!less?outputStyle=expanded&sourceMap')
-        }, {
-            test: /\.(png|jpg)$/,
-            loaders: ['url-loader?limit=8192']
-        }] // inline base64 URLs for <=8k images, direct URLs for the rest
-    },
-    plugins: [
-        new ExtractTextPlugin("styles.css"),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(env)
-        }),
-        HtmlWebpackPluginConfig,
-        CopyWebpackPluginConfig
-    ]
+		// This is required for webpack-dev-server. The path should
+		// be an absolute path to the build destination.
+		outputPath: path.join(`${__dirname}/`),
+		stats: {
+			chunks: false
+		}
+	},
+	devtool: 'source-map',
+	module: {
+		preLoaders: [ {
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loaders: [ 'eslint-loader' ]
+		} ],
+		loaders: [ {
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loaders: [ 'react-hot', 'babel-loader' ]
+		}, {
+			test: /\.(css|less)$/,
+			exclude: [ /src(\\|\/)js/ ],
+			loader: ExtractTextPlugin.extract('style-loader', 'css?sourceMap!less?sourceMap')
+		}, {
+			test: /\.(css|less)$/,
+			include: [ /src(\\|\/)js/ ],
+			loader: ExtractTextPlugin.extract(
+				'style-loader',
+				'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!less?outputStyle=expanded&sourceMap')
+		}, {
+			test: /\.(png|jpg)$/,
+			loaders: [ 'file-loader?name=assets/[hash].[ext]' ]
+		} ] // inline base64 URLs for <=8k images, direct URLs for the rest
+	},
+	plugins: [
+		new ExtractTextPlugin('styles.css'),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify(env)
+		}),
+		HtmlWebpackPluginConfig,
+		CopyWebpackPluginConfig
+	]
 }
 
 if (env === 'production') {
-    config.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false }
-        })
-    )
+	config.plugins.push(
+		new webpack.optimize.UglifyJsPlugin({
+			compress: { warnings: false }
+		})
+	)
 }
 
 if (env !== 'production') {
-    config.plugins.push(
+	config.plugins.push(
 		new webpack.HotModuleReplacementPlugin()
-    )
+	)
 }
 
 // use export default
-module.exports = config;
+module.exports = config
