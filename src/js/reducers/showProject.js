898a76f9ce5e6b777bcfc5afe4d2cf0c2503efe3
Jamie Allen <Jamieallen59@gmail.com>
2018-08-21 20:19:58 +0100

    updated Readme | updated summary | updated skills

--- src/js/reducers/showProject.js
+++ src/js/reducers/showProject.js
@@ -2,6 +2,13 @@ import { createSelector } from 'reselect'
 
 const SELECT_TAB = 'SELECT_TAB'
 
+const getSelected = state => state.selected
+
+export const getSelectedProject = createSelector(
+	getSelected,
+	selected => selected
+)
+
 const showProject = (state = {}, action) => {
 	switch (action.type) {
 		case SELECT_TAB:
@@ -15,10 +22,3 @@ const showProject = (state = {}, action) => {
 }
 
 export default showProject
-
-const getSelected = state => state.selected
-
-export const getSelectedProject = createSelector(
-	getSelected,
-	selected => selected
-)
